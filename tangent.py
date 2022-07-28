a1931a378f37e2ad8c69b7cdc5fd99befb79557f
med <93465561+tfwnof@users.noreply.github.com>
2022-07-28 23:48:59 +0400

    Update tangent.py

--- tangent.py
+++ tangent.py
@@ -1,15 +1,9 @@
-#Incomplete
-
 def tangent(Cx,Cy, Px,Py):
     "where (Cx, Cy) are the circle's center coordinates and (Px,Py) is the point we're trying to find the tangent to"
     #finding the slope and intercept of the two coordinates
     slope = (Cy - Py)/(Cx - Px)
     intercept = -(Cx*slope)+Cy
-    
-    #finding the inverse formula
-    #invSlope = 1/slope
-    #invIntercept = Cx - (Cy/slope)
-    
+    #finding the slope and intercept for the tangent line
     tanSlope = -1/slope
     tanIntercept = Py - tanSlope*Px
     
