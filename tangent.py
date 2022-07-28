0219ed394dd3a3e941189b1bf3acf3ad165f27cd
med <93465561+tfwnof@users.noreply.github.com>
2022-07-28 23:40:34 +0400

    Update tangent.py

--- tangent.py
+++ tangent.py
@@ -7,9 +7,10 @@ def tangent(Cx,Cy, Px,Py):
     intercept = -(Cx*slope)+Cy
     
     #finding the inverse formula
-    invSlope = 1/slope
-    invIntercept = Cx - (Cy/slope)
+    #invSlope = 1/slope
+    #invIntercept = Cx - (Cy/slope)
     
-
+    tanSlope = -1/slope
+    tanIntercept = Py - tanSlope*Px
     
-    return slope, intercept, invSlope, invIntercept
+    return tanSlope, tanIntercept
