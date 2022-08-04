aedb6c810b7a4cab92e64232915e8e91e21bd125
med <93465561+tfwnof@users.noreply.github.com>
2022-08-05 02:01:25 +0400

    Update 2-Even_Fibonacci_numbers.py

--- ProjectEuler/Solved/2-Even_Fibonacci_numbers.py
+++ ProjectEuler/Solved/2-Even_Fibonacci_numbers.py
@@ -1,4 +1,6 @@
 # Problem 2
+# Even Fibonacci numbers
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
 a,b,c = 1,2,0
 while c <= 4000000:
